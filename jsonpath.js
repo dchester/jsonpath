381953d3a5aa351d1f6dc9a2811f69e49bcb509d
David Chester <david@fmail.co.uk>
2016-05-14 15:16:21 -0400

    bump version to 0.2.3

--- jsonpath.js
+++ jsonpath.js
@@ -1,4 +1,4 @@
-/*! jsonpath 0.2.2 */
+/*! jsonpath 0.2.3 */
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.jsonpath = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"./aesprim":[function(require,module,exports){
 /*
@@ -4758,7 +4758,8 @@ Handlers.prototype._fns['member-child-numeric_literal'] =
 	Handlers.prototype._fns['subscript-child-numeric_literal'];
 
 Handlers.prototype._fns['member-descendant-numeric_literal'] =
-	Handlers.prototype._fns['member-descendant-identifier'];
+    Handlers.prototype._fns['subscript-descendant-string_literal'] =
+    Handlers.prototype._fns['member-descendant-identifier'];
 
 function eval_recurse(partial, src, template) {
 
@@ -5790,6 +5791,9 @@ var currentQueue;
 var queueIndex = -1;
 
 function cleanUpNextTick() {
+    if (!draining || !currentQueue) {
+        return;
+    }
     draining = false;
     if (currentQueue.length) {
         queue = currentQueue.concat(queue);
